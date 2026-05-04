9e50748d186e99f79ab59ab4329e0c13fb838188
Chuongdinh <quocchuong277@gmail.com>
2026-05-04 08:55:52 +0700

    +add UIManager

--- Assets/_Game/Scripts/Manager/UIManager.cs
+++ Assets/_Game/Scripts/Manager/UIManager.cs
@@ -1,19 +1,66 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public void UpdateScoreText()
+    public static UIManager Instance;
+
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject gamePlayPanel;
+    public GameObject settingPanel;
+
+
+    [Header("Gameplay Elements")]
+    public TextMeshProUGUI currentLevelText;
+
+    [Header("Setting Elements")]
+    public TextMeshProUGUI levelTextSetting;
+    public TextMeshProUGUI brickCountText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnPlayButtonClick()
+    {
+        mainMenuPanel.SetActive(false);
+        gamePlayPanel.SetActive(true);
+        GameManager.Instance.StartGame();
+    }
+
+    public void OnSettingButtonClick()
+    {
+        Debug.Log("đã bấm button");
+        settingPanel.SetActive(true);
+        Time.timeScale = 0;
+
+        levelTextSetting.text = "LEVEL" + GameManager.Instance.currentLevel;
+        brickCountText.text = GameManager.Instance.score.ToString();
+    }
+
+    public void OnCloseSettingClick()
+    {
+        settingPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ToggleSound()
     {
         
     }
 
-    public void ShowWinPanel()
+    public void UpdateCurrentLevel(int level)
     {
-        
-    }
-
-    public void ShowLosePanel()
-    {
-        
+        currentLevelText.text = "LEVEL" + level;
     }
 }
